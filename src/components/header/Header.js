95ca9b2d7f9e541da62a9f8dbf612408bdef7804
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-01-19 14:18:14 -0600

    Created a very rough implementation of the landing page. -Added landing page. -Added animation for header -Added placeholders for images

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,56 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/core/Menu";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <h2><a href="#">Mountain Traveling</a></h2>
-            <nav>
-                <li><a href="#">SignIn/SignUp</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </nav>
-            </header>
-        )
+    constructor(props) {
+        super(props);
     }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              TaskBoard
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-export default Header;
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(Header);
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,18 +5,55 @@ import "./Landing.css";
 class Landing extends Component {
   render() {
     return (
-      <section class="hero">
-        <div className="background-image"/>
-        <div class="hero-content-area">
-          <h1>TaskBoard</h1>
-          <h3>Tasks prioritized like a pro.</h3>
-          <a href="#" class="btn">
-            Sign up now!
-          </a>
-        </div>
-      </section>
+      <div>
+        <HeroSection />
+        <ExampleSection />
+      </div>
     );
   }
+}
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <div className="background-image"/>
+      <div className="hero-content-area">
+        <h1>TaskBoard</h1>
+        <h3>Tasks prioritized like a pro.</h3>
+        <a href="#" class="btn">
+          Sign up now!
+        </a>
+      </div>
+    </section>
+  )
+}
+
+const ExampleSection = () => {
+  const img1 = {
+    backgroundImage: "url(http://www.hotelalexzermatt.com/wp-content/uploads/2015/05/cr-visualimpact.ch-Rainer-Eder-1.jpg)"
+  }
+  const img2 = {
+    backgroundImage: "url(https://wallpapercave.com/wp/cYQcruq.jpg)"
+  }
+  const img3 = {
+    backgroundImage: "url(http://www.redresort.com/wp-content/uploads/2016/04/tabphoto-summer-1.jpg)"
+  }
+  const img4 = {
+    backgroundImage: "url(https://www.thephotoargus.com/wp-content/uploads/2013/01/mountain-02.jpg)"
+  }
+  return (
+    <section className="destinations">
+      <h3 className="title">Get the best out of your day</h3>
+      <p>Organizing has never been easier</p>
+      <hr/>
+      <ul class="grid">
+        <li className="large" style={img2}></li>
+          <li className="small" style={img1}></li>
+          <li className="small" style={img3}></li>
+          <li className="large" style={img4}></li>
+      </ul>
+    </section>
+  )
 }
 
 export default Landing;

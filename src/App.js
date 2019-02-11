47d3aa2b8d37dba68351d781b44c629711488e77
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-02-10 22:43:15 -0600

    Major changes
    - Removed material ui, did not know how to use
    - Added materialize css 
    - Added new login page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       <div>
         <Header />
         <Main />
-        <Footer />
       </div>
     );
   }
--- src/components/signin/SignInForm.js
+++ src/components/signin/SignInForm.js
@@ -1,93 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
+import React from "react";
+import "./SignInForm.css";
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
-
-function SignIn(props) {
-  const { classes } = props;
-
-  return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input name="password" type="password" id="password" autoComplete="current-password" />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign in
-          </Button>
-        </form>
-      </Paper>
-    </main>
-  );
+class Logo extends React.Component {
+  render() {
+    return (
+      <div className="logo">
+        <i className="fa fa-bug" aria-hidden="true" />
+        <span> Login </span>
+      </div>
+    );
+  }
 }
 
-SignIn.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+class Input extends React.Component {
+  render() {
+    return (
+      <div className="Input">
+        <input
+          type={this.props.type}
+          name={this.props.name}
+          placeholder={this.props.placeholder}
+          required
+          autocomplete="false"
+        />
+        <label for={this.props.name} />
+      </div>
+    );
+  }
+}
 
-export default withStyles(styles)(SignIn);
+// Modal
+class Modal extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className="Modal">
+        <Logo />
+        <form onSubmit={this.props.onSubmit}>
+          <Input type="text" name="username" placeholder="username" />
+          <Input type="password" name="password" placeholder="password" />
+          <button> Sign In</button>
+        </form>
+        <div className="social-signin">
+          <button className="fb" onClick={this.props.onClick}>
+            <i className="fa fa-facebook" aria-hidden="true" />
+          </button>
+          <button className="tw" onClick={this.props.onClick}>
+            <i className="fa fa-twitter" aria-hidden="true" />
+          </button>
+        </div>
+        <a href="#">Lost your password ?</a>
+      </div>
+    );
+  }
+}
+
+
+class SignInForm extends React.Component {
+  render() {
+    return (
+      <div className="background">
+        <Modal onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+export default SignInForm;

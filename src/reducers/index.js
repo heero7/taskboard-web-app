dcd6f90cbbee5dc8d07163ada8aaa6a081946a7b
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-02-14 22:45:30 -0600

    Added reducer index file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,14 @@
-import { alertConstants } from "../constants";
+import { combineReducers } from "redux";
 
-export function alert(state = {}, action) {
-    switch(action.type) {
-        case alertConstants.SUCCESS:
-            return {
-                type : "alert-success",
-                message : action.message
-            };
-        case alertConstants.FAILURE:
-            return {
-                type : "alert-danger",
-                message : action.message
-            };
-        case alertConstants.CLEAR:
-            return {};
-        default:
-            return state;
-    }
-}
+import { authentication } from "./auth.reducer";
+import { registration } from "./register.reducer";
+//import { users } from "./users.reducer";
+import { alert } from "./alert.reducer";
+
+const rootReducer = combineReducers({
+    authentication,
+    registration,
+    alert
+});
+
+export default rootReducer;

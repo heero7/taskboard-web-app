b9f71943894cd1a4ca1eb9ebc68288aa93a34f61
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-02-27 23:34:42 -0600

    Render tasks with a function
    - Render tasks by priority
    - TaskCard gets whole task object

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,23 +7,63 @@ import ColumnHeader from "../reusables/ColumnHeader";
 import TaskCard from "../tasks/TaskCard";
 
 class Dashboard extends React.Component {
+  constructor(props) {
+      super(props);
+    this.state = {
+      tasks: [
+        {
+          taskName: "Do Laundrysfsfafasdfasdfsafsdfaasfsfasfsasf",
+          taskPriority: 3
+        },
+        {
+          taskName: "Do homework",
+          taskPriority: 2
+        },
+        {
+          taskName: "Voting study",
+          taskPriority: 1
+        },
+        {
+          taskName: "Yoga",
+          taskPriority: 1
+        },
+        {
+          taskName: "Workout",
+          taskPriority: 0
+        }
+      ]
+    };
+  }
+
+  renderPriorityTasks(priority) {
+    if (!this.state.tasks || priority > 3) return <h5>All done for now!</h5>; 
+    let highPrioirity = this.state.tasks.filter(task => task.taskPriority === priority);
+    return highPrioirity.map((task, key) => {
+      return <TaskCard task={task} key={key} />;
+    });
+  }
+
+  
   render() {
     return (
       <div>
-        <h1>Tasks</h1>
+        <h2>Welcome, User!</h2>
         <Row>
-          <Col m={4} s={12}>
-            <ColumnHeader title={"High"}/>
-            <TaskCard taskName={"Do Laundrysfsfafasdfasdfsafsdfaasfsfasfsasf "} taskPriority={"High"}/>
+          <Col m={3} s={12}>
+            <ColumnHeader title={"High"} />
+            {this.renderPriorityTasks(3)}
           </Col>
-          <Col m={4} s={12}>
-            <ColumnHeader title={"Medium"}/>
-            <TaskCard taskName={"Do homework "} taskPriority={"Medium"}/>
+          <Col m={3} s={12}>
+            <ColumnHeader title={"Medium"} />
+            {this.renderPriorityTasks(2)}
           </Col>
-          <Col m={4} s={12}>
-            <ColumnHeader title={"Low"}/>
-            <TaskCard taskName={"Voting study"} taskPriority={"Low"}/>
-            <TaskCard taskName={"Yoga"} taskPriority={"Low"}/>
+          <Col m={3} s={12}>
+          <ColumnHeader title={"Low"} />
+          {this.renderPriorityTasks(1)}
+          </Col>
+          <Col m={3} s={12}>
+            <ColumnHeader title={"Done"} />
+            {this.renderPriorityTasks(0)}
           </Col>
         </Row>
       </div>
--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -5,47 +5,62 @@ import { Card, Button } from "react-materialize";
 import "../styles/TaskCard.css";
 import "../styles/Button.css";
 
+const Priority = {
+  0: { value: "Done", textColor : "" },
+  1: { value: "Low", textColor : "" },
+  2: { value: "Medium", textColor : "" },
+  3: { value: "High", textColor : "" }
+};
+
 class TaskCard extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  //todo: pass whole task as prop so when 
+  //todo: pass whole task as prop so when
   //clicking on edit it can update by id
 
   render() {
+    let priorityLvl = this.props.task.taskPriority;
     return (
-        <Card className="task-card" textClassName="white-text">
-          <div className="content-container">
-            <div className="content">
-              <h5 className="task-card-text-wrapper">Task: {this.props.taskName}</h5>
-              <h5 className="task-card-text-wrapper">Priority: {this.props.taskPriority}</h5>
-            </div>
-            <div className="task-card-buttons">
-              <Button
-              floating medium
-                id="complete-button"
-                className="action-button"
-                waves="light"
-                icon="done"
-              />
-              <Button
-              floating medium
-                id="edit-button"
-                className="action-button"
-                waves="light"
-                icon="edit"
-              />
-              <Button
-              floating medium
-                id="delete-button"
-                className="action-button"
-                waves="light"
-                icon="delete"
-              />
-            </div>
+      <Card className="task-card" textClassName="white-text">
+        <div className="content-container">
+          <div className="content">
+            <h5 className="task-card-text-wrapper">
+              Task: {this.props.task.taskName}
+            </h5>
+            <h5 className="task-card-text-wrapper">
+              Priority: {Priority[priorityLvl].value}
+            </h5>
           </div>
-        </Card>
+          <div className="task-card-buttons">
+            <Button
+              floating
+              medium
+              id="complete-button"
+              className="action-button"
+              waves="light"
+              icon="done"
+            />
+            <Button
+              floating
+              medium
+              id="edit-button"
+              className="action-button"
+              waves="light"
+              icon="edit"
+            />
+            <Button
+              floating
+              medium
+              id="delete-button"
+              className="action-button"
+              waves="light"
+              icon="delete"
+            />
+          </div>
+        </div>
+      </Card>
     );
   }
 }
